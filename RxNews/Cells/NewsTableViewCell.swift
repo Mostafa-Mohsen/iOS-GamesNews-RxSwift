2bcb32515399a50c9bba6db77b55aeb282910c50
Mostafa-Mohsen <eng.mostafamohsen2016@gmail.com>
2023-09-25 01:53:01 +0300

    add tableview cell

---
+++ RxNews/Cells/NewsTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewsTableViewCell.swift
+//  RxNews
+//
+//  Created by User on 18/12/2022.
+//
+
+import Foundation
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
